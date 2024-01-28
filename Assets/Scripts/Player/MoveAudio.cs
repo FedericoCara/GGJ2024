3a8c522913ddf322d188085397821a958173b8e1
federico.cara <federico.cara@etermax.com>
2024-01-28 14:56:51 -0300

    Added walking and running sounds

---
+++ Assets/Scripts/Player/MoveAudio.cs
@@ -0,0 +1,32 @@
+using FMODUnity;
+using UnityEngine;
+
+
+public class MoveAudio : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private EventReference walkAudioStep;
+    [SerializeField] private EventReference runAudioStep;
+    private const float walkSpeed = 0.7f;
+    private const float minRunSpeed = 0.9f;
+
+    public void PlayWalkStep()
+    {
+        if(GetCurrentSpeed()<=walkSpeed)
+        {
+            Debug.Log("PlayWalkStep");
+            RuntimeManager.PlayOneShot(walkAudioStep);
+        }
+    }
+
+    public void PlayRunStep()
+    {
+        if (GetCurrentSpeed() >= minRunSpeed)
+        {
+            Debug.Log("PlayRunStep");
+            RuntimeManager.PlayOneShot(runAudioStep);
+        }
+    }
+
+    private float GetCurrentSpeed() => animator.GetFloat(GenericBehaviour.SpeedParameterHash);
+}
